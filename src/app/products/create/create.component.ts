f3d0be1ef20772c1f3caec937d20cd7105882db4
FreeIntelligence <free04381@gmail.com>
2023-10-19 19:14:26 -0300

    Se completa el boceto del formulario de registro.

--- src/app/products/create/create.component.ts
+++ src/app/products/create/create.component.ts
@@ -1,13 +1,18 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators } from '@angular/forms';
-import { markFormAsTouched } from 'src/utils/utils';
+import {
+  addDays,
+  currentDate,
+  markFormAsTouched,
+  randomString,
+} from 'src/utils/utils';
 
 @Component({
   selector: 'app-create',
   templateUrl: './create.component.html',
   styleUrls: ['./create.component.scss'],
 })
-export class CreateComponent {
+export class CreateComponent implements OnInit {
   form = new FormGroup({
     id: new FormControl('', [
       Validators.required,
@@ -25,9 +30,40 @@ export class CreateComponent {
       Validators.maxLength(200),
     ]),
     logo: new FormControl('', [Validators.required]),
-    date_release: new FormControl('', [Validators.required]),
-    date_revision: new FormControl('', [Validators.required]),
+    date_release: new FormControl(currentDate(), [Validators.required]),
+    date_revision: new FormControl(
+      {
+        value: '',
+        disabled: true,
+      },
+      [Validators.required]
+    ),
   });
+
+  ngOnInit(): void {
+    this.form.controls.id.setValue(randomString(10));
+    this.setDateRevision();
+  }
+
+  setDateRevision() {
+    const newDateRevision = this.calcDateRevision();
+
+    if (newDateRevision) {
+      this.form.controls.date_revision.setValue(newDateRevision);
+    }
+  }
+
+  calcDateRevision() {
+    const dateRelease = this.form.controls.date_release.value;
+
+    if (dateRelease) {
+      const newDateRevision = addDays(dateRelease, 365);
+
+      return newDateRevision;
+    }
+
+    return null;
+  }
 
   submit() {
     if (this.form.invalid) {
--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -6,3 +6,22 @@ export const markFormAsTouched = (form: FormGroup) => {
     control?.markAsTouched();
   });
 };
+
+export const currentDate = () => {
+  return new Date().toISOString().substring(0, 10);
+};
+
+export const addDays = (date: string, days: number) => {
+  const d = new Date(date);
+  d.setDate(d.getDate() + days);
+  return d.toISOString().substring(0, 10);
+};
+
+export const randomString = (length: number) => {
+  const chars =
+    '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  let result = '';
+  for (let i = length; i > 0; --i)
+    result += chars[Math.floor(Math.random() * chars.length)];
+  return result;
+};
